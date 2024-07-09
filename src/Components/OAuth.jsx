4850ff314f094c51a7bda55e886d2f0bb7e761cb
IBARAM1917 <abiraminagaraj22@gmail.com>
2024-07-09 10:07:28 +0530

    completed

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -16,7 +16,7 @@ const OAuth = () => {
     provider.setCustomParameters({ prompt: "select_account" });
     try {
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch("http://localhost:3000/api/auth/google", {
+      const res = await fetch("https://backend-1-1bsn.onrender.com/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
